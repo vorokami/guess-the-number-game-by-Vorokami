095209e9cea464bae3c22ba014dc0a8b82eeff84
vorokami <vorokami@gmail.com>
2021-11-18 12:36:34 +0300

    Adding project to repository

---
+++ console/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming;
+
+import academy.learnprogramming.config.GameConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@Slf4j
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("Guess The Number Game");
+
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -0,0 +1,33 @@
+package academy.learnprogramming;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class NumberGeneratorImpl implements NumberGenerator {
+
+    // == fields ==
+    private final Random random = new Random();
+
+    @Getter
+    private final int maxNumber;
+
+    @Getter
+    private final int minNumber;
+
+    // == constructors ==
+    public NumberGeneratorImpl(@MaxNumber int maxNumber, @MinNumber int minNumber) {
+        this.maxNumber = maxNumber;
+        this.minNumber = minNumber;
+    }
+
+    // == public methods ==
+    @Override
+    public int next() {
+        return (random.nextInt(maxNumber-minNumber+1) + minNumber);
+    }
+
+}
